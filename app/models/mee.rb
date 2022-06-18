b5c2bf5ebfae427adfaffddb460ad337f5055fd0
kijima-line <marugo2792@gmail.com>
2022-06-18 20:26:08 +0900

    win_ok

--- app/models/mee.rb
+++ app/models/mee.rb
@@ -1,3 +1,5 @@
 class Mee < ApplicationRecord
-    yes
+    belongs_to :user
+    validates :q_a,presence: true, allow_blank: true, numericality: {only_integer: true}, length: { in: 1..12 }
+    validates :q_b,presence: true, allow_blank: true, numericality: {only_integer: true}, length: { in: 1..31 }
 end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         has_many :mees
+         validates :name,presence: true, length: { maximum: 7 }
+end
