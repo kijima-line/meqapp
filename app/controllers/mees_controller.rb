d50383d1f1c85ad7896e2b6efffc5751d78cea69
jun <97328580+kijima-line@users.noreply.github.com>
2022-06-18 18:31:51 +0900

    mees

--- app/controllers/mees_controller.rb
+++ app/controllers/mees_controller.rb
@@ -1,2 +1,46 @@
 class MeesController < ApplicationController
-end
+    def index
+      @mees = Mee.order("created_at DESC").includes(:user)
+    end
+  
+    def new
+      @mee = Mee.new 
+    end
+  
+  
+    def create
+      mee = Mee.new(mee_params)
+      if mee.save
+        redirect_to root_path
+      else
+        @mees = Mee.all
+        render :new
+      end
+    end
+  
+    def destroy
+      @mee = Mee.find(params[:id])
+    if @mee.destroy
+      redirect_to root_path
+  
+    end
+    end
+    def edit
+      @mee = Mee.find(params[:id])
+    end
+    def update
+      @mee = Mee.find(params[:id])
+     if @mee.update(mee_params)
+      redirect_to root_path
+     end
+    end
+    def show
+      @mee = Mee.find(params[:id])
+    end
+  
+    private
+    def mee_params
+      params.require(:mee,).permit(:q_a, :q_b, :q_1, :q_2, :q_3, :q_4, :q_5, :q_6, :q_7).merge(user_id: current_user.id)
+    end
+  end
+  
