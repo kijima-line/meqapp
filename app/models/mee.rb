5fb8dbc3041ea6e3c8bf4372a1ffe44d95640d13
kijima-line <marugo2792@gmail.com>
2022-06-22 19:04:58 +0900

    titleバリテーション

--- app/models/mee.rb
+++ app/models/mee.rb
@@ -1,7 +1,7 @@
 class Mee < ApplicationRecord
     belongs_to :user
     # has_one_attached :image
-    #validates :title
+    validates :title,presence: true
     validates :q_a, allow_blank: true, numericality: {only_integer: true}, length: { in: 1..12 },presence:true
     validates :q_b, allow_blank: true, numericality: {only_integer: true}, length: { in: 1..31 } ,presence:true
 end
